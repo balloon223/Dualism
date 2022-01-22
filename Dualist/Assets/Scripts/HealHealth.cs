6f06a5c661ceedc1fe853b37d37b5002e2d5c177
balloon223 <bryanleng223@gmail.com>
2022-01-22 17:56:06 -0500

    -Heal Health function -Heal Spirit function

---
+++ Dualist/Assets/Scripts/HealHealth.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealHealth : MonoBehaviour
+{
+    public bool isColliding;
+    public int damage = -25;
+    PlayerStats playerStats;
+
+    private void Start()
+    {
+        playerStats = FindObjectOfType<PlayerStats>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        isColliding = true;
+    }
+
+    private void Update()
+    {
+        if (isColliding)
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                playerStats.TakeHealthDamage(damage);
+            }
+        }
+    }
+}
